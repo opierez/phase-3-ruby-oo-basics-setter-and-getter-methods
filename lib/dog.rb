a3ff606663046964f2b8771557fd8363871484ba
olivia <opierez@gmail.com>
2023-01-09 17:07:30 -0500

    commit tree 0122d03447e880853bbce4340aeeda5fead1f550

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,23 @@
 class Dog
 
+    def name=(name)
+        @name = name
+    end
+
+    def name 
+        @name
+    end
+
+    def breed=(breed)
+        @breed = breed
+    end
+
+    def breed
+        @breed
+    end
+
 end
+
+delilah = Dog.new
+delilah.name = "Delilah"
+puts delilah.name 
